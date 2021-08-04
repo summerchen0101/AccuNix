3de4b8b5d5b80d0cf1507d7f7cbfa4f5bb8ad92d
Summer Chen <mp4bj6ruby@gmail.com>
2021-08-04 16:11:08 +0800

    Home: CardPanel component

---
+++ src/components/CardPanel.vue
@@ -0,0 +1,28 @@
+<script lang="tsx">
+import { defineComponent, toRefs } from "vue";
+export default defineComponent({
+  name: "CardPanel",
+  props: {
+    title: String,
+    icon: String,
+    desc: String,
+  },
+  setup(props) {
+    const { title, icon, desc } = toRefs(props);
+    return () => (
+      <div class="bg-white rounded-md p-4 shadow-md">
+        <div class="flex items-center">
+          <div class="flex-1 space-x-2 flex items-center">
+            <i class={["text-gray-400 text-xl", icon.value].join(" ")}></i>
+            <span class="text-lg text-gray-600 font-bold">{title.value}</span>
+          </div>
+          <span class="bg-gray-200 text-gray-500 py-1 px-3 text-sm rounded-full cursor-pointer">
+            設定
+          </span>
+        </div>
+        <div class="text-gray-500 text-sm mt-3 pb-3">{desc.value}</div>
+      </div>
+    );
+  },
+});
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,7 @@ import { defineComponent, reactive, ref } from "vue";
 import Layout from "@/components/Layout/Layout.vue";
 import TabGroup from "@/components/TabGroup.vue";
 import { OptionType } from "@/types";
+import CardPanel from "@/components/CardPanel.vue";
 
 export default defineComponent({
   name: "Home",
@@ -14,11 +15,45 @@ export default defineComponent({
     ]);
     return () => (
       <Layout>
-        <TabGroup
-          value={currentTab.value}
-          onChange={(value: number) => (currentTab.value = value)}
-          options={tabOptions}
-        />
+        <div class="py-3">
+          <TabGroup
+            value={currentTab.value}
+            onChange={(value: number) => (currentTab.value = value)}
+            options={tabOptions}
+          />
+        </div>
+        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+          <CardPanel
+            title="自動回應設定"
+            icon="far fa-caret-square-left"
+            desc="提供 LINE 自動回應訊息設定。"
+          />
+          <CardPanel
+            title="關鍵字庫"
+            icon="fab fa-slack-hash"
+            desc="提供 LINE 關鍵字互動，可藉由關鍵字動作：貼標籤、發送訊息、切換主選單、進入腳本、呼叫真人客服。"
+          />
+          <CardPanel
+            title="排程任務"
+            icon="far fa-clock"
+            desc="可指定時間發送訊息或切換主選單。"
+          />
+          <CardPanel
+            title="好友管理"
+            icon="far fa-user"
+            desc="可以查看與搜尋 LINE 好友。"
+          />
+          <CardPanel
+            title="常用訊息庫"
+            icon="far fa-comments"
+            desc="提供 LINE 常用訊息庫設定，支援多種樣式靈活調整。"
+          />
+          <CardPanel
+            title="問卷腳本"
+            icon="far fa-sticky-note"
+            desc="可透過問卷腳本，與問卷管理對接，完成好友個人基本資料或愛好蒐集。"
+          />
+        </div>
       </Layout>
     );
   },
