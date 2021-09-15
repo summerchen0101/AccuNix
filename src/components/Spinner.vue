da5fcc454997701966cfaf608a84edb73ac5de6f
Summer Chen <mp4bj6ruby@gmail.com>
2021-09-15 17:04:01 +0800

    Spinner style updated

--- src/components/Spinner.vue
+++ src/components/Spinner.vue
@@ -5,7 +5,7 @@ export default defineComponent({
   setup() {
     return () => (
       <div class="flex items-center justify-center text-gray-500 h-full min-h-[100px]">
-        <i class="fas fa-spinner fa-spin fa-3x opacity-80"></i>
+        <i class="fas fa-spinner animate-spin fa-3x opacity-80"></i>
       </div>
     );
   },
