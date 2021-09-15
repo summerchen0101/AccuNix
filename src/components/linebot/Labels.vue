203c2ccafbc2a71fbd77423cb95d2e660ab097bf
Summer Chen <mp4bj6ruby@gmail.com>
2021-09-15 17:57:45 +0800

    Labels: 1. use setup mode, 2. api

--- src/components/linebot/Labels.vue
+++ src/components/linebot/Labels.vue
@@ -1,58 +1,63 @@
-<script lang="tsx">
+<script lang="ts" setup>
 import PageHeader from "@/components/Layout/PageHeader.vue";
 import SectionPanel from "@/components/SectionPanel.vue";
-import { defineComponent, reactive, ref } from "vue";
-
-export default defineComponent({
-  name: "LinebotLabels",
-  components: {
-    PageHeader,
-  },
-  setup() {
-    const selected = ref<keyof typeof tableMap>(1);
-    const limit = ref(10);
-    const tableData = reactive(
-      [...Array(5)].map(() => ({
-        label: "積極買家",
-        count: 12,
-      }))
-    );
-    const tableMap = {
-      [1]: (
-        <el-table data={tableData} stripe class="w-100" size="small">
-          <el-table-column prop="label" label="標籤"></el-table-column>
-          <el-table-column prop="count" label="人數"></el-table-column>
-        </el-table>
-      ),
-      [2]: (
-        <el-table data={tableData} stripe class="w-100" size="small">
-          <el-table-column prop="label" label="標籤"></el-table-column>
-          <el-table-column prop="count" label="次數"></el-table-column>
-        </el-table>
-      ),
-    };
-
-    return () => (
-      <SectionPanel title="整體標籤概況">
-        {{
-          default: () => [
-            <div class="mt-3">
-              <el-radio-group class="mb-3" v-model={selected.value}>
-                <el-radio label={1}>依人數呈現</el-radio>
-                <el-radio label={2}>依觸發次數呈現</el-radio>
-              </el-radio-group>
-              {tableMap[selected.value]}
-            </div>,
-          ],
-          plus: () => [
-            <el-select v-model={limit.value} size="mini" class="w-24">
-              <el-option label="前10標籤" value={10} />
-              <el-option label="前20標籤" value={20} />
-            </el-select>,
-          ],
-        }}
-      </SectionPanel>
-    );
-  },
+import useTagOverview from "@/service/useTagOverview";
+import { onMounted, reactive, ref } from "vue";
+const selected = ref(1);
+const limit = ref(10);
+const { list, fetchData } = useTagOverview();
+onMounted(() => {
+  fetchData();
 });
+// const tableData_1 = reactive(
+//   [...Array(5)].map(() => ({
+//     label: "積極買家1",
+//     count: 12,
+//   }))
+// );
+// const tableData_2 = reactive(
+//   [...Array(5)].map(() => ({
+//     label: "積極買家2",
+//     count: 10,
+//   }))
+// );
 </script>
+
+<template>
+  <SectionPanel title="整體標籤概況">
+    <template v-slot:default>
+      <div class="mt-3">
+        <el-radio-group class="mb-3" v-model="selected">
+          <el-radio :label="1">依人數呈現</el-radio>
+          <el-radio :label="2">依觸發次數呈現</el-radio>
+        </el-radio-group>
+        <el-table
+          :data="list"
+          stripe
+          class="w-100 h-96 overflow-y-auto"
+          size="small"
+          max-height="250"
+        >
+          <el-table-column prop="name" label="標籤"></el-table-column>
+          <el-table-column
+            v-if="selected === 1"
+            prop="user_count"
+            label="人數"
+          ></el-table-column>
+          <el-table-column
+            v-else
+            prop="active_count"
+            label="次數"
+          ></el-table-column>
+        </el-table>
+      </div>
+    </template>
+
+    <template v-slot:plus>
+      <el-select v-model="limit" size="mini" class="w-24">
+        <el-option label="前10標籤" value="{10}" />
+        <el-option label="前20標籤" value="{20}" />
+      </el-select>
+    </template>
+  </SectionPanel>
+</template>
---
+++ src/service/useTagOverview.ts
@@ -0,0 +1,47 @@
+import { useApiErrHandler } from "@/hooks/useApiErrHandler";
+import useRequest from "@/hooks/useRequest";
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import { useLineBotState } from "./../providers/lineBotProvider";
+
+export interface Tag {
+  name: string;
+  user_count: number;
+  user_percentage: string;
+  active_count: number;
+  active_percentage: string;
+}
+
+export interface TagOverviewRes {
+  data: Tag[];
+  message?: string;
+}
+
+function useTagOverview() {
+  const router = useRouter();
+  const apiErrHandler = useApiErrHandler();
+  const { lineBotGuid } = useLineBotState();
+  const isLoading = ref(false);
+  const isError = ref(false);
+  const list = ref<TagOverviewRes["data"]>([]);
+  const fetchData = async () => {
+    isLoading.value = true;
+    isError.value = false;
+    try {
+      const res = await useRequest<TagOverviewRes>({
+        method: "get",
+        url: `LINEBot/${lineBotGuid.value}/dashboard/tag-overview`,
+      });
+      list.value = res.data;
+    } catch (err) {
+      apiErrHandler(err);
+      isError.value = true;
+    }
+    isLoading.value = false;
+    return list.value;
+  };
+
+  return { list, fetchData, isLoading };
+}
+
+export default useTagOverview;
