44a6dcc99f9fd72492fb2c907f9510edf1f1efb7
Summer Chen <mp4bj6ruby@gmail.com>
2021-09-16 00:11:21 +0800

    Keywords api

--- src/components/linebot/Keywords.vue
+++ src/components/linebot/Keywords.vue
@@ -1,70 +1,75 @@
-<script lang="tsx">
-import PageHeader from '@/components/Layout/PageHeader.vue'
+<script lang="ts" setup>
 import SectionPanel from '@/components/SectionPanel.vue'
-import { defineComponent, reactive, ref } from 'vue'
+import useKeywordTrand from '@/service/useKeywordTrend'
+import { subDays, format } from 'date-fns'
+import { reactive, ref, onMounted } from 'vue'
+import Spinner from '../Spinner.vue'
 
-interface Columns {
-  keyword: string
-  percent: number
-  count: number
-  compare: string
+const startAt = ref(subDays(new Date(), 8))
+const endAt = ref(subDays(new Date(), 1))
+const { list, fetchData, isLoading } = useKeywordTrand()
+
+const onSearch = () => {
+  const search = {
+    startAt: startAt.value ? format(startAt.value, 'yyyy-MM-dd') : undefined,
+    endAt: endAt.value ? format(endAt.value, 'yyyy-MM-dd') : undefined,
+  }
+  fetchData(search)
 }
-export default defineComponent({
-  name: 'LinebotKeywords',
-  components: {
-    PageHeader,
-  },
-  setup() {
-    const tableData = reactive<Columns[]>(
-      [...Array(5)].map(() => ({
-        keyword: '天竺鼠車車',
-        percent: 48,
-        count: 12,
-        compare: '-',
-      })),
-    )
 
-    return () => (
-      <SectionPanel title="關鍵字趨勢">
-        {{
-          default: () => [
-            <div class="mt-3">
-              <div class="flex space-x-3 mb-3">
-                <el-date-picker
-                  type="daterange"
-                  unlink-panels
-                  range-separator="~"
-                  start-placeholder="开始日期"
-                  end-placeholder="结束日期"
-                  size="small"
-                ></el-date-picker>
-              </div>
-              <el-table data={tableData} stripe class="w-100" size="small">
-                <el-table-column
-                  prop="keyword"
-                  label="關鍵字"
-                ></el-table-column>
-                <el-table-column label="整體佔比" align="center">
-                  {{
-                    default: ({ row }: { row: Columns }) => [
-                      <span>{row.percent}%</span>,
-                    ],
-                  }}
-                </el-table-column>
-                <el-table-column
-                  prop="compare"
-                  label="與上區段對比"
-                ></el-table-column>
-                <el-table-column
-                  prop="count"
-                  label="觸發次數"
-                ></el-table-column>
-              </el-table>
-            </div>,
-          ],
-        }}
-      </SectionPanel>
-    )
-  },
+onMounted(() => {
+  onSearch()
 })
 </script>
+
+<template>
+  <SectionPanel title="關鍵字趨勢">
+    <template v-slot:default>
+      <div class="mt-3 min-h-[300px]">
+        <div class="flex space-x-2 mb-3">
+          <el-date-picker
+            type="date"
+            size="small"
+            v-model="startAt"
+            placeholder="開始日期"
+          ></el-date-picker>
+          <span>~</span>
+          <el-date-picker
+            type="date"
+            size="small"
+            v-model="endAt"
+            placeholder="結束日期"
+          ></el-date-picker>
+          <el-button type="primary" size="small" @click="onSearch">
+            查詢
+          </el-button>
+        </div>
+        <Spinner v-if="isLoading" />
+        <template v-else>
+          <el-table
+            :data="list"
+            stripe
+            class="w-100"
+            size="small"
+            max-height="250"
+          >
+            <el-table-column prop="keyword" label="關鍵字"></el-table-column>
+            <el-table-column label="整體佔比" align="center">
+              <template #default="props">
+                <span>{{ props.row.overall }}%</span>
+              </template>
+            </el-table-column>
+            <el-table-column
+              prop="compared"
+              label="與上區段對比"
+            ></el-table-column>
+            <el-table-column
+              prop="totalActiveCount"
+              label="觸發次數"
+            ></el-table-column>
+          </el-table>
+        </template>
+      </div>
+    </template>
+  </SectionPanel>
+</template>
---
+++ src/service/useKeywordTrend.ts
@@ -0,0 +1,53 @@
+import { useApiErrHandler } from '@/hooks/useApiErrHandler'
+import useRequest from '@/hooks/useRequest'
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { useLineBotState } from '../providers/lineBotProvider'
+
+export interface KeywordTrandReq {
+  startAt?: string
+  endAt?: string
+}
+
+export interface KeywordTrand {
+  id: number
+  keyword: string
+  totalActiveCount: number
+  overall: number
+  compared: number
+}
+
+export interface KeywordTrandRes {
+  data: KeywordTrand[]
+  message?: string
+}
+
+function useKeywordTrand() {
+  const router = useRouter()
+  const apiErrHandler = useApiErrHandler()
+  const { lineBotGuid } = useLineBotState()
+  const isLoading = ref(false)
+  const isError = ref(false)
+  const list = ref<KeywordTrand[]>([])
+  const fetchData = async (req: KeywordTrandReq) => {
+    isLoading.value = true
+    isError.value = false
+    try {
+      const res = await useRequest<KeywordTrandRes>({
+        method: 'get',
+        url: `LINEBot/${lineBotGuid.value}/dashboard/keyword-trend`,
+        config: { params: req },
+      })
+      list.value = res.data
+    } catch (err) {
+      apiErrHandler(err)
+      isError.value = true
+    }
+    isLoading.value = false
+    return list.value
+  }
+
+  return { list, fetchData, isLoading }
+}
+
+export default useKeywordTrand
