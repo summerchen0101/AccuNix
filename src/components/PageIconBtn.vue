053ef467ae108c63562d7688093d979acc6fae35
Summer Chen <mp4bj6ruby@gmail.com>
2021-10-27 13:26:27 +0800

    PageIconBtn: tsx to ts

--- src/components/PageIconBtn.vue
+++ src/components/PageIconBtn.vue
@@ -1,5 +1,7 @@
-<script lang="tsx">
-import { defineComponent, PropType } from 'vue'
+<script lang="ts">
+import { defineComponent, toRefs, computed, PropType } from 'vue'
+
+type ColorTypes = 'danger' | 'success' | 'secondary'
 
 export default defineComponent({
   props: {
@@ -8,20 +10,36 @@ export default defineComponent({
       default: 'fas fa-check',
     },
     color: {
-      type: String,
-      default: 'gray',
+      type: String as PropType<ColorTypes>,
+      default: 'secondary',
     },
-    onClick: Function as PropType<() => void>,
   },
   setup(props) {
-    return () => (
-      <div
-        class={`text-white rounded-sm inline-flex items-center justify-center cursor-pointer h-8 w-8 text-sm bg-${props.color}-500 hover:bg-${props.color}-400`}
-        onClick={props.onClick}
-      >
-        <i class={props.iconClass}></i>
-      </div>
-    )
+    const colorClassMap: Record<ColorTypes, string> = {
+      secondary: 'bg-gray-500 hover:bg-gray-400',
+      danger: 'bg-red-500 hover:bg-red-400',
+      success: 'bg-green-500 hover:bg-green-400',
+    }
+    return { colorClassMap }
   },
 })
 </script>
+
+<template>
+  <div
+    class="
+      text-white
+      rounded-sm
+      inline-flex
+      items-center
+      justify-center
+      cursor-pointer
+      h-8
+      w-8
+      text-sm
+    "
+    :class="colorClassMap[color]"
+  >
+    <i :class="iconClass"></i>
+  </div>
+</template>
