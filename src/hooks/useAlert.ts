8c8c3171b54d6c045fbccacfe86df51b43c394ea
Summer Chen <mp4bj6ruby@gmail.com>
2021-11-25 22:25:54 +0800

    useAlert: force only show one message

--- src/hooks/useAlert.ts
+++ src/hooks/useAlert.ts
@@ -2,8 +2,11 @@ import { ElMessage } from 'element-plus'
 
 type MessageType = '' | 'success' | 'warning' | 'info' | 'error'
 function useAlert() {
-  const alert = (message: string, type: MessageType) =>
+  const alert = (message: string, type: MessageType) => {
+    ElMessage.closeAll()
     ElMessage({ type, message })
+  }
+
   return alert
 }
 
