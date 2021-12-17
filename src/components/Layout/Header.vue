e95076dff440e42c008b1dfbba6042fe320daa41
Summer Chen <mp4bj6ruby@gmail.com>
2021-12-17 22:07:15 +0800

    Header added user info dropdown

--- src/components/Layout/Header.vue
+++ src/components/Layout/Header.vue
@@ -1,18 +1,72 @@
 <template>
-  <div class="p-3 bg-white flex items-center h-14 fixed top-0 w-screen z-20">
-    <div class="w-64">
+  <div class="bg-white flex items-center h-14 fixed top-0 w-screen z-20">
+    <div class="w-64 p-3">
       <img class="w-36 h-auto" src="@/assets/logo.png" alt="" />
     </div>
-    <div class="flex-1 flex justify-end md:justify-between">
+    <div class="flex-1 flex items-center justify-end h-full">
       <i
         class="round-btn round-btn-static text-gray-400 fas fa-bars"
         @click="isMiniSidebar = !isMiniSidebar"
       >
       </i>
-      <i
+      <div class="flex-1"></div>
+      <div
+        class="
+          flex
+          items-center
+          justify-center
+          border-l
+          h-full
+          cursor-pointer
+          relative
+          group
+          w-52
+        "
+      >
+        <span class="el-avatar el-avatar--circle"
+          ><img src="https://blb.accubot.com.tw/uploads/!logged-user.jpg"
+        /></span>
+        <div class="text-center w-28">
+          <div class="">Accunix</div>
+          <div class="text-gray-500 text-sm">maintainer</div>
+        </div>
+        <i class="el-icon-arrow-down"></i>
+        <div class="pt-14 invisible absolute top-0 right-0 group-hover:visible">
+          <div
+            class="
+              border
+              bg-white
+              h-32
+              w-52
+              transition-all
+              scale-y-0
+              group-hover:scale-y-100
+              origin-top
+              text-gray-400 text-sm
+              flex flex-col
+              gap-4
+              p-4
+            "
+          >
+            <div class="hover:text-gray-500">
+              <i class="fas fa-user-cog"></i>
+              編輯個人資料
+            </div>
+            <div class="hover:text-gray-500">
+              <i class="fa fa-download"></i>
+              下載紀錄
+            </div>
+            <div class="hover:text-gray-500">
+              <i class="fas fa-power-off"></i>
+              登出
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- <i
         class="fas fa-sign-out-alt round-btn round-btn-static hidden md:flex"
         @click="handleLogout"
-      ></i>
+      ></i> -->
     </div>
   </div>
 </template>
