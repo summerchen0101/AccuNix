92e425a5c4e96ec1e970092eb6c8bb43ecd43939
Summer Chen <mp4bj6ruby@gmail.com>
2022-03-10 14:52:42 +0800

    use funtional pina store

--- src/service/store/botStore.ts
+++ src/service/store/botStore.ts
@@ -1,41 +1,66 @@
 import { productApiPathMap } from '@/lib/maps'
 import { defineStore } from 'pinia'
+import { computed, ref } from 'vue'
 import { Bot, LoginInfoRes, Organization } from '../api/useLoginInfo'
 
-interface LoginUser {
+export interface LoginUser {
   id: number
   name: string
   picture: string
 }
-interface BotState {
-  bot: Bot | null
-  org: Organization | null
-  botList: Bot[]
-  user: LoginUser | null
-}
 
-export const useBotStore = defineStore('botStore', {
-  state: (): BotState => ({
-    bot: null,
-    org: null,
-    botList: [],
-    user: null,
-  }),
-  getters: {
-    orgGuid: (state) => state.org.GUID,
-    botType: (state) => state.bot.product_type_id,
-    botGuid: (state) => state.bot.GUID,
-    botApiPath: (state) => (state.bot ? `${productApiPathMap[state.bot.product_type_id]}/${state.bot.GUID}` : ''),
-  },
-  actions: {
-    gotLoginInfo(info: LoginInfoRes) {
-      this.org = info.organization
-      this.user = {
-        id: info.id,
-        name: info.name,
-        picture: info.picture,
-      }
-      this.bot = info.bots[0]
-    },
-  },
+export const useBotStore = defineStore('botStore', () => {
+  const bot = ref<Bot>()
+  const org = ref<Organization>()
+  const botList = ref<Bot[]>([])
+  const user = ref<LoginUser>()
+
+  const gotLoginInfo = (info: LoginInfoRes) => {
+    org.value = info.organization
+    botList.value = info.bots
+    bot.value = info.bots[0]
+    user.value = {
+      id: info.id,
+      name: info.name,
+      picture: info.picture,
+    }
+  }
+  return {
+    bot,
+    org,
+    botList,
+    user,
+
+    orgGuid: computed(() => org.value?.GUID),
+    botGuid: computed(() => bot.value?.GUID),
+    botType: computed(() => bot.value?.product_type_id),
+    botApiPath: computed(() => (bot.value ? `${productApiPathMap[bot.value.product_type_id]}/${bot.value.GUID}` : '')),
+
+    gotLoginInfo,
+  }
 })
+// export const useBotStore = defineStore('botStore', {
+//   state: (): BotState => ({
+//     bot: null,
+//     org: null,
+//     botList: [],
+//     user: null,
+//   }),
+//   getters: {
+//     orgGuid: (state) => state.org.GUID,
+//     botType: (state) => state.bot.product_type_id,
+//     botGuid: (state) => state.bot.GUID,
+//     botApiPath: (state) => (state.bot ? `${productApiPathMap[state.bot.product_type_id]}/${state.bot.GUID}` : ''),
+//   },
+//   actions: {
+//     gotLoginInfo(info: LoginInfoRes) {
+//       this.org = info.organization
+//       this.user = {
+//         id: info.id,
+//         name: info.name,
+//         picture: info.picture,
+//       }
+//       this.bot = info.bots[0]
+//     },
+//   },
+// })
