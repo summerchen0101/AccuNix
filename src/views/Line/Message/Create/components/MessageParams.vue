ac83ee93ceead6bf94a91505e67c72debe44e8c9
Summer Chen <mp4bj6ruby@gmail.com>
2022-03-18 11:36:20 +0800

    MessageParams upgrade

--- src/views/Line/Message/Create/components/MessageParams.vue
+++ src/views/Line/Message/Create/components/MessageParams.vue
@@ -1,43 +1,50 @@
 <template>
-  <div class="relative group inline-block">
-    <div
-      class="
-        bg-white
-        shadow-sm
-        border border-gray-200
-        rounded-full
-        w-7
-        h-7
-        grid
-        place-content-center
-        text-gray-500
-        cursor-pointer
-      "
-    >
-      { }
-    </div>
-    <div
-      class="
-        absolute
-        top-0
-        left-0
-        bg-white
-        border border-gray-50
-        shadow-lg
-        rounded-md
-        z-10
-        p-3
-        -ml-32
-        mt-9
-        w-48
-        hidden
-        group-hover:block
-      "
-    >
-      <div>選擇參數</div>
-      <div v-for="{ label, code } in fixedParams" :key="code" class="flex gap-3 items-center py-1">
-        <div class="text-gray-400">{{ label }}</div>
-        <div class="bg-primary-400 text-white p-0.5 px-1 rounded">{{ code }}</div>
+  <div :tabindex="1" @focus.capture="onFocus" @blur.capture="onBlur">
+    <slot></slot>
+  </div>
+  <div class="absolute top-0 right-0 mr-2 message-params transition-all">
+    <div class="relative group inline-block">
+      <div
+        class="
+          bg-white
+          shadow-sm
+          border border-gray-200
+          rounded-full
+          w-7
+          h-7
+          grid
+          place-content-center
+          text-gray-500
+          cursor-pointer
+        "
+      >
+        { }
+      </div>
+      <div
+        class="
+          absolute
+          top-0
+          left-0
+          bg-white
+          border border-gray-50
+          shadow-lg
+          rounded-md
+          z-10
+          p-3
+          -ml-32
+          mt-9
+          w-48
+          hidden
+          group-hover:block
+          leading-normal
+          text-sm
+        "
+      >
+        <div>選擇參數</div>
+        <div v-for="{ label, code } in fixedParams" :key="code" class="flex gap-3 items-center py-1 mt-1">
+          <div class="text-gray-400">{{ label }}</div>
+          <div class="bg-primary-400 text-white p-0.5 px-1 rounded">{{ code }}</div>
+        </div>
       </div>
     </div>
   </div>
@@ -56,7 +63,17 @@ export default defineComponent({
     const fixedParams = computed(() =>
       Object.entries(props.params).map(([code, label]) => ({ label, code: `{{${code}}}` })),
     )
-    return { fixedParams }
+    const onFocus = (e: FocusEvent) => {
+      ;((e.target as Element).closest('.el-form-item') as Element)
+        .querySelector('.message-params')
+        .classList.add('-mr-9')
+    }
+    const onBlur = (e: FocusEvent) => {
+      ;((e.target as Element).closest('.el-form-item') as Element)
+        .querySelector('.message-params')
+        .classList.remove('-mr-9')
+    }
+    return { fixedParams, onFocus, onBlur }
   },
 })
 </script>
